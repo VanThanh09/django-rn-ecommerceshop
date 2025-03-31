0a69869dcb4697c32f8234e6b511274024ab5224
Thanh <dangthanh.090904@gmail.com>
2025-03-31 14:03:13 +0700

    merge main

--- eshopapis/admin.py
+++ eshopapis/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Store)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Attribute)
+admin.site.register(AttributeValue)
+admin.site.register(ProductVariant)
+admin.site.register(OrderDetail)
+admin.site.register(Cart)
+admin.site.register(Comment)
+admin.site.register(Rating)
+
